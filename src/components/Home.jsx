b443521b393feb9688980e4d5d9ef8fd23c93323
MetalB <metalb311@gmail.com>
2023-01-23 22:10:11 -0700

    all things working

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,9 @@ import RifleImg from '../images/rifle.jpg'
 import ShotgunImg from '../images/shotgun.jpg'
 import Header from '../components/Header'
 
+
 // import PistolImg from 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fblueridgebrass.com%2Fproduct-tag%2Fbulk-reloading-bullets%2F&psig=AOvVaw37ANdiQ-2nKcsihVLB2OC_&ust=1674233274858000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCPClvY-L1PwCFQAAAAAdAAAAABAJ'
+
 
 const Home = () => {
   return (
