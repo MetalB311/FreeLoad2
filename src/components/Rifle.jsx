e8a261e1046c87a555a84b4592cf43eadda39de1
MetalB <metalb311@gmail.com>
2023-01-25 17:34:26 -0700

    all images uploaded

--- src/components/Rifle.jsx
+++ src/components/Rifle.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 import Header from '../components/Header'
+import Img223 from '../images/223.jpg'
+import Img308 from '../images/308.jpg'
 
 const Rifle = () => {
     return (
@@ -12,13 +14,19 @@ const Rifle = () => {
           <button>Home</button>
           </Link> */}
 
-          <Link to='/Remington'>
-          <button>223 Remington</button>
-          </Link>
+        <a href="/Remington" onclick="event.preventDefault()">223Remington<img src={Img223} alt='223Remington' height='300px' width='300px'></img>
+        </a>
 
-          <Link to='/Winchester'>
+        <a href="/Winchester" onclick="event.preventDefault()">308Winchester<img src={Img308} alt='308Winchester' height='300px' width='300px'></img>
+        </a>
+
+          {/* <Link to='/Remington'>
+          <button>223 Remington</button>
+          </Link> */}
+
+          {/* <Link to='/Winchester'>
           <button>308 Winchester</button>
-          </Link>
+          </Link> */}
 
 
 
